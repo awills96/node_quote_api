6ba4953b9e45448971d8b353a5fb996517fdd8bd
awills96 <vtalex2014@gmail.com>
2022-05-05 10:12:34 -0400

    Implemented all the routes

--- server.js
+++ server.js
@@ -8,3 +8,35 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+app.listen(PORT)
+
+app.get('/api/quotes/random', (req, res, next) => {
+    quoteObj = getRandomElement(quotes);
+    res.send({
+        quote: quoteObj
+    })
+})
+
+app.get('/api/quotes', (req, res, next) => {
+    if (!req.query.person){
+        res.send({
+            quotes: quotes
+        });
+    } else {
+        let result = quotes.filter(quote => quote.person === req.query.person)
+        res.send({
+            quotes: result
+        })
+    }
+});
+
+app.post('/api/quotes', (req, res, next) => {
+    if (req.query.quote && req.query.person){
+        quotes.push(req.query);
+        res.send({
+            quote: req.query
+        })
+    } else {
+        res.status(400).send();
+    }
+});
